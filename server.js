3ad8356743a6d4e511a58dc7d7f15d2b0ba09551
silencer929 <petermbugua929@gmail.com>
2024-06-20 17:05:45 +0300

    serve file update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
-require('dotenv').config();
+// require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
